62c793cec428eb3a819205b8aa7db6623da4fd52
mylastof <mylastof@gmail.com>
2015-10-22 13:24:26 +0700

    master
    master data now using CI 3.0.2

--- system/juragan/views/user/pesanan/pesanan_view.php
+++
@@ -1,8 +0,0 @@
-<div class="container-fluid">
-	<!-- start .page-content -->
-	<div class="page-content">
-		<?php echo $this->load->view('user/inc/menu_satu'); ?>
-		<div class="" id="loader"></div>
-	</div>
-	<!-- end .page-content -->
-</div>    
