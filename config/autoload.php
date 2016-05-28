d5edf17341908f2e7dcf0dd60142c8ce7a681b07
mylastof <mylastof@gmail.com>
2016-05-28 18:45:06 +0700

    satu
    bikin project baru

--- config/autoload.php
+++ config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'database', 'pagination', 'table');
+$autoload['libraries'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -72,6 +72,12 @@ $autoload['libraries'] = array('session', 'form_validation', 'database', 'pagina
 | Prototype:
 |
 |	$autoload['drivers'] = array('cache');
+|
+| You can also supply an alternative property name to be assigned in
+| the controller:
+|
+|	$autoload['drivers'] = array('cache' => 'cch');
+|
 */
 $autoload['drivers'] = array();
 
@@ -83,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('fungsi', 'url', 'string', 'date');
+$autoload['helper'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -126,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('setting_model' => 'setting', 'login_model' => 'login', 'juragan_model' => 'juragan', 'pesanan_model' => 'pesanan');
+$autoload['model'] = array();
--- config/routes.php
+++ config/routes.php
@@ -19,7 +19,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | Please see the user guide for complete details:
 |
-|	http://codeigniter.com/user_guide/general/routing.html
+|	https://codeigniter.com/user_guide/general/routing.html
 |
 | -------------------------------------------------------------------------
 | RESERVED ROUTES
@@ -49,13 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'login';
-
-$route['admin'] = 'admin/pesanan/semua';
-$route['administrator/(:any)'] = "admin/default_c/$1";
-
-$route['user'] = 'user/default_c';
-$route['user/(:any)'] = "user/default_c/$1";
-
+$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
