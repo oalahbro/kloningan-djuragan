4d0d69a41c9778e63be44338fcb3687168aff7b7
mylastof <mylastof@gmail.com>
2016-06-02 16:58:41 +0700

    update session page

--- application/controllers/admin/Pesanan.php
+++ application/controllers/admin/Pesanan.php
@@ -5,11 +5,11 @@ class Pesanan extends CI_Controller {
 
 	public function __construct() {
     	parent::__construct();
-    	if(is_login() === TRUE) {
-			if((is_user_level('admin') === TRUE) || (is_user_level('superadmin') === TRUE)) {
-				redirect('admin');
-			}
-			else {
+    	if(is_login() !== TRUE) {
+    		redirect('login');
+    	}
+    	else {
+			if(is_user_level('user') === TRUE) {
 				redirect('juragan');
 			}
 		}
