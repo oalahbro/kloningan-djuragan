ad08c8e40fc698e5df5f8bb4476f0258b0938c09
mylastof <mylastof@gmail.com>
2016-06-08 15:23:00 +0700

    update folder app

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/webjuragan/';
+$config['base_url'] = 'http://localhost/webjuragan/order-juragan';
 
 /*
 |--------------------------------------------------------------------------
