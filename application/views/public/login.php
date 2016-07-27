b4a04cf2166fb5385e2215b66d16e4664f4415a8
mylastof <mylastof@gmail.com>
2016-07-27 10:10:01 +0700

    update jQuery version to v1.12.4

--- application/views/public/login.php
+++ application/views/public/login.php
@@ -60,7 +60,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </div>
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 
