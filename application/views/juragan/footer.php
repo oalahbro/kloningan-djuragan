a3d007edc83745c781c11f453b40345178496e08
mylastof <mylastof@gmail.com>
2016-10-10 17:39:10 +0700

    load local jquery

--- application/views/juragan/footer.php
+++ application/views/juragan/footer.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
 
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
