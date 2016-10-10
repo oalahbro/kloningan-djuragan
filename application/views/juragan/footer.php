eb9364aa578a0829d48c32f59c2f0fb92336eec8
mylastof <mylastof@gmail.com>
2016-10-10 11:14:22 +0700

    whitespaces

--- application/views/juragan/footer.php
+++ application/views/juragan/footer.php
@@ -134,43 +134,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script type="text/javascript">
 		(function ($) {
 			// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('#scrolll').outerHeight();
+			var didScroll;
+			var lastScrollTop = 0;
+			var delta = 5;
+			var navbarHeight = $('#scrolll').outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
-});
+			$(window).scroll(function(event){
+				didScroll = true;
+			});
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
+			setInterval(function() {
+				if (didScroll) {
+					hasScrolled();
+					didScroll = false;
+				}
+			}, 250);
 
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('#scrolll').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('#scrolll').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
-}
+			function hasScrolled() {
+				var st = $(this).scrollTop();
+				
+				// Make sure they scroll more than delta
+				if(Math.abs(lastScrollTop - st) <= delta)
+					return;
+				
+				// If they scrolled down and are past the navbar, add class .nav-up.
+				// This is necessary so you never see what is "behind" the navbar.
+				if (st > lastScrollTop && st > navbarHeight){
+					// Scroll Down
+					$('#scrolll').removeClass('nav-down').addClass('nav-up');
+				} else {
+					// Scroll Up
+					if(st + $(window).height() < $(document).height()) {
+						$('#scrolll').removeClass('nav-up').addClass('nav-down');
+					}
+				}
+				
+				lastScrollTop = st;
+			}
 
 
 		})(jQuery);
