dc02c05e887ecb62a869ffddad5de80e6e9cb38d
mylastof <mylastof@gmail.com>
2016-10-10 12:47:50 +0700

    update var$

--- application/controllers/juragan/Pesanan.php
+++ application/controllers/juragan/Pesanan.php
@@ -4,8 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pesanan extends CI_Controller {
 
+	var $s_id;
+
 	public function __construct() {
     	parent::__construct();
+
+    	$this->s_id = $this->session->userdata('juragan')['id_juragan'];
+
     	if(is_login() !== TRUE) {
     		redirect('login'); // belum masuk ? dialihkan ke halaman `login`
     	}
@@ -24,8 +29,6 @@ class Pesanan extends CI_Controller {
 		$nama = $this->juragan->ambil_nama_by_id($array_auth[0]);
 		$url_nama = url_title($nama, '-', TRUE);
 
-		$s_id = $this->session->userdata('juragan')['id_juragan']; // ambil data session id_juragan
-
 		if($juragan_id !== 0) {
 			if(in_array($juragan_id, $array_auth)) { // set session juragan dari pilihan daftar allow_id
 				$sess_array = array(
@@ -36,7 +39,7 @@ class Pesanan extends CI_Controller {
 			}
 		}
 		else {
-			if( ! isset($s_id)) { // set session juragan dari pertama login, diambil dari daftar pertama
+			if( ! isset($this->s_id)) { // set session juragan dari pertama login, diambil dari daftar pertama
 				$sess_array = array(
 					'id_juragan' => $array_auth[0],
 					'warna_juragan' => _warna($url_nama)
@@ -49,12 +52,23 @@ class Pesanan extends CI_Controller {
 
     // fungsi tulis data pesanan baru
     public function tulis_baru() {
+    	$juragan = _nama_juragan($this->s_id);
+
+    	$this->data = array(
+			'title' => 'Tulis Pesanan - ' . $juragan . ' | ' . $this->config->item('site_name'),
+			'active' => 'pesanan',
+			'juragan' => $juragan
+			);
+
+		$this->load->view('juragan/header', $this->data);
+		$this->load->view('juragan/menu', $this->data);
+		$this->load->view('juragan/pesanan/dasbor', $this->data);
+		$this->load->view('juragan/footer', $this->data);
     }
 
     // fungsi menampilkan daftar pesanan 
     public function daftar($u = '') {
-    	$data = $this->session->userdata('juragan')['id_juragan'];
-    	$juragan = _nama_juragan($data);
+    	$juragan = _nama_juragan($this->s_id);
 
     	$this->data = array(
 			'title' => 'Pesanan - ' . $juragan . ' | ' . $this->config->item('site_name'),
