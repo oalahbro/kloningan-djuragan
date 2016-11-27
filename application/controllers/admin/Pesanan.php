8ef8af571489088c7c58337162557835f9315ac4
mylastof <mylastof@gmail.com>
2016-11-27 16:10:44 +0700

    (admin) tambah session login

--- application/controllers/admin/Pesanan.php
+++ application/controllers/admin/Pesanan.php
@@ -3,6 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pesanan extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+    	if(is_login() === TRUE) {
+			if((_is_user_level('user') === TRUE)) {
+				redirect('juragan');
+			}
+		}
+		else {
+			redirect('masuk');
+		}
+	}
+
 	public function lihat($juragan = 'semua', $status = 'semua') {
 		$data = array(
 			'title' => 'Pesanan'
