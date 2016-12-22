1cf1da549310e2e2b75cad9a3a7b0d5a52b7a754
mylastof <mylastof@gmail.com>
2016-12-22 12:01:50 +0700

    tambah fungsi cloning form

--- application/views/admin/pesanan/write.php
+++ application/views/admin/pesanan/write.php
@@ -420,6 +420,141 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		}, false);
 
+
+		/////////// CLONER
+		$(function () {
+			$('#btnAdd_2').click(function () {
+				var num     = $('.clonedInput_2').length,
+				newNum  = new Number(num + 1), 
+				maxNum  = 2,
+				newElem = $('#phone' + num).clone().attr('id', 'phone' + newNum).fadeIn('slow');
+
+				newElem.find('.number_phone').val('');
+
+				$('#phone' + num).after(newElem);
+				$('#ID' + newNum + '_title').focus();
+
+				$('#btnDel_2').attr('disabled', false).show('slow');
+
+				if (newNum == maxNum)
+					$('#btnAdd_2').attr('disabled', true).hide('slow');
+			});
+
+			$('#btnDel_2').click(function () {
+				swal({
+					title: "Kamu yakin akan menghapus?",
+					text: "Kolom HP 2 akan dihapus dan tidak dapat dibatalkan",
+					type: "warning",
+					animation: "slide-from-top",
+					showCancelButton: true,
+					confirmButtonColor: "#DD6B55",
+					confirmButtonText: "Yup, hapus!",
+					cancelButtonText: "Tidak",
+					closeOnConfirm: false,
+					closeOnCancel: true
+				},
+				function(isConfirm){
+					if (isConfirm) {
+						var num = $('.clonedInput_2').length;
+						$('#phone' + num).slideUp('slow', function () {
+							$(this).remove();
+							if (num -1 === 1)
+								$('#btnDel_2').attr('disabled', true).hide('slow');
+							$('#btnAdd_2').attr('disabled', false).show('slow');
+						});
+
+						swal({
+							title: "Dihapus!",
+							text: "Kolom HP 2 sudah dihapus",
+							timer: 1100,
+							type: "success",
+							showConfirmButton: false
+						});
+					}
+				});
+			});
+			$('#btnAdd_2').attr('disabled', false).show('slow');
+			$('#btnDel_2').attr('disabled', true).hide('slow');
+
+
+			///////////////////// PESANAN /////////////////////
+
+			$('#btnAdd').click(function () {
+
+				var n = $('.update').length,
+					nn = new Number(n + 2);
+
+					html = '<div class="row clonedInput berijarakbawah" id="entry' + nn + '">';
+						html += '<div class="col-sm-4 update">';
+							html += '<div class="input-group">';
+								html += '<span class="input-group-addon">Kode</span>';
+								html += '<input type="text" name="kode[]" value="" id="kode" class="form-control kode typeahead" placeholder="kode" required="required" autocomplete="off" />';
+							html += '</div>';
+						html += '</div>';
+						html += '<div class="col-sm-4">';
+							html += '<div class="input-group">';
+								html += '<span class="input-group-addon">Ukuran</span>';
+								html += '<input type="text" name="size[]" value="" id="size" class="form-control size" placeholder="size" required="required" autocomplete="off" />';
+							html += '</div>';
+						html += '</div>';
+
+						html += '<div class="col-sm-4">';
+							html += '<div class="input-group">';
+								html += '<span class="input-group-addon">Jumlah</span>';
+								html += '<input type="number" name="jumlah[]" value="" id="jumlah" class="form-control jumlah" placeholder="jumlah" required="required" autocomplete="off" min="1" />';
+							html += '</div>';
+						html += '</div>';
+					html += '</div>';
+
+				$('#btnDel').attr('disabled', false).show('slow');
+				$('#ngok').append(html).show('slow');
+				
+
+				createTypeahead('#entry' + nn + ' .typeahead', 'aa'+nn);
+
+				$('#entry' + nn + ' .kode').focus();
+			});
+
+			$('#btnDel').click(function () {
+				swal({
+					title: "Kamu yakin akan menghapus?",
+					text: "Kolom pesanan terakhir akan dihapus dan tidak dapat dibatalkan",
+					type: "warning",
+					animation: "slide-from-top",
+					showCancelButton: true,
+					confirmButtonColor: "#DD6B55",
+					confirmButtonText: "Yup, hapus!",
+					cancelButtonText: "Tidak",
+					closeOnConfirm: false,
+					closeOnCancel: true
+				},
+				function(isConfirm){
+					if (isConfirm) {
+						var num = $('.clonedInput').length;
+						$('#entry' + num).slideUp('slow', function () {
+							$(this).remove();
+
+							if (num -1 === 1)
+								$('#btnDel').attr('disabled', true).hide('slow');
+
+							$('#btnAdd').attr('disabled', false).show('slow');
+						});
+
+						swal({
+							title: "Dihapus!",
+							text: "Kolom pesanan terakhir sudah dihapus",
+							timer: 1100,
+							type: "success",
+							showConfirmButton: false
+						});
+					}
+				});
+			});
+
+			$('#btnAdd').attr('disabled', false).show('slow');
+			$('#btnDel').attr('disabled', true).hide('slow');
+		});
+
 		////// TYPEAHEAD
 
 		var databank = new Bloodhound({
