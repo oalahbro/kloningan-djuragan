c74d04d92f22ed73edfd9290bc1306b740a19d42
mylastof <mylastof@gmail.com>
2016-12-22 15:43:25 +0700

    tambah footer

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -3,8 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 		</div>
 	</div>
+	<footer class="footer">
+		<div class="container-fluid">
+			<span class="text-muted">Place sticky footer content here.</span>
+		</div>
+	</footer>
 	<!-- jQuery first, then Tether, then Bootstrap JS. -->	
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 
 </body>
