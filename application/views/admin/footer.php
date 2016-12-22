27aab58c532856feaef0c7fec236b61e5dffae90
mylastof <mylastof@gmail.com>
2016-12-22 15:55:09 +0700

    FOOTER - update footer

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -5,10 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
 	<footer class="footer">
 		<div class="container-fluid">
-			<span class="text-muted">Place sticky footer content here.</span>
+			<div class="credit">&copy; 2014 - 2016 Pesanan <span class="text-muted">by Toto</span></div>
 		</div>
 	</footer>
-	<!-- jQuery first, then Tether, then Bootstrap JS. -->	
+	<!-- jQuery first, then Tether, then Bootstrap JS. -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 
