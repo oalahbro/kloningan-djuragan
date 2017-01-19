22e3393f5771bc9758982298f27b3f532ba24589
mylastof <mylastof@gmail.com>
2017-01-19 21:00:17 +0700

    update view reset password

--- application/views/publik/v_lupa.php
+++ application/views/publik/v_lupa.php
@@ -1,23 +1,25 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<div class="col-md-6 offset-md-3">
-    <div class="card mx-2">
-        <div class="card-block p-2">
-            <h1>Lupa Sandi</h1>
-            <p class="text-muted">Untuk reset sandi, silakan isi form berikut.</p>
-            <?php 
-                echo form_open();
+<div class="card-group mb-0">
+	<div class="card p-2">
+		<div class="card-block">
+			<div class="card-block">
+				<h1>Lupa Sandi</h1>
+				<p class="text-muted">Untuk reset sandi, silakan isi form berikut.</p>
+				<?php 
+				echo form_open();
 
-                echo '<div class="input-group mb-1">';
-                echo '<span class="input-group-addon">@</span>';
-                echo form_input(array('name' => 'username', 'placeholder' => 'Username', 'class' => 'form-control'));
-                echo '</div>';
+				echo '<div class="input-group mb-1">';
+				echo '<span class="input-group-addon">@</span>';
+				echo form_input(array('name' => 'username', 'placeholder' => 'Username', 'class' => 'form-control'));
+				echo '</div>';
 
-                echo form_button(array('class' => 'btn btn-block btn-success', 'content' => 'Daftar'));
+				echo form_button(array('class' => 'btn btn-block btn-success', 'content' => 'Reset'));
 
-                echo form_close();
-            ?>
-        </div>
-    </div>
+				echo form_close();
+				?>
+			</div>
+		</div>
+	</div>
 </div>
