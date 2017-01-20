ae1dcd9156b6be091fbb29a97095dda9a987c757
mylastof <mylastof@gmail.com>
2017-01-20 16:10:41 +0700

    whitespaces

--- application/views/admin/pesanan/show.php
+++ application/views/admin/pesanan/show.php
@@ -4,55 +4,57 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <style type="text/css">
 	#searchclear {
-    position: absolute;
-    right: 25px;
-    top: 10px;
-    cursor: pointer;
-    color: #ccc;
-}
-#searchclear:hover {
-	color: #222;
-}
-
+		position: absolute;
+		right: 25px;
+		top: 10px;
+		cursor: pointer;
+		color: #ccc;
+	}
+	#searchclear:hover {
+		color: #222;
+	}
+	.dataTables_filter {
+		display: none;
+	}
 </style>
-    <!-- Main content -->
-    <main class="main">
-    	<ol class="breadcrumb" id="tableH">
-            <li class="breadcrumb-item">Home</li>
-            <?php 
-            $i = 0;
-            $act = '';
-            foreach ($breadcrumb as $key => $value) {
-            	$i++;
-            	if($i === count($breadcrumb)) {
-            		$act = ' active';
-            	}
-            	echo '<li class="breadcrumb-item' . $act . '">'.anchor($key, $value).'</li>';
-            }
-            ?>
-        </ol>
+		<!-- Main content -->
+		<main class="main">
+			<ol class="breadcrumb" id="tableH">
+						<li class="breadcrumb-item">Home</li>
+						<?php 
+						$i = 0;
+						$act = '';
+						foreach ($breadcrumb as $key => $value) {
+							$i++;
+							if($i === count($breadcrumb)) {
+								$act = ' active';
+							}
+							echo '<li class="breadcrumb-item' . $act . '">'.anchor($key, $value).'</li>';
+						}
+						?>
+				</ol>
 
-        <div class="kontener">
-        	<div class="card">
-        		<div class="card-block">
-	        		<div class="row">
-	        			<div class="col-sm-2">
-	        				<select name='length_change' id='length_change' class="form-control">
-		        				<option value='5'>5 data</option>
-		        				<option value='25' selected="">25 data</option>
-	        					<option value='50'>50 data</option>
-	        					<option value='75'>75 data</option>
-	        					<option value='100'>100 data</option>
-	        				</select>
-	        			</div>
-	        			<div class="col-sm-3 offset-sm-7">
-		        			<div class="form-group">
-		        				<input type="text" class="form-control" placeholder="cari data" id="myInputTextField">
-		        				<span id="searchclear" class="icon-close"></span>
-	        				</div>
-	        			</div>
-	        		</div>
-        			<table id="example" class="table table-border table-hover" cellspacing="0" width="100%">
+				<div class="kontener">
+					<div class="card">
+						<div class="card-block">
+							<div class="row">
+								<div class="col-sm-2">
+									<select name='length_change' id='length_change' class="form-control">
+										<option value='5'>5 data</option>
+										<option value='25' selected="">25 data</option>
+										<option value='50'>50 data</option>
+										<option value='75'>75 data</option>
+										<option value='100'>100 data</option>
+									</select>
+								</div>
+								<div class="col-sm-3 offset-sm-7">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="cari data" id="myInputTextField">
+										<span id="searchclear" class="icon-close"></span>
+									</div>
+								</div>
+							</div>
+							<table id="example" class="table table-border table-hover" cellspacing="0" width="100%">
 						<thead>
 							<tr>
 								<th>#</th>
@@ -67,94 +69,94 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							</tr>
 						</thead>
 					</table>
-        		</div>
-        	</div>
+						</div>
+					</div>
 
 
-        </div>
-        <!-- /.conainer-fluid -->
-    </main>
+				</div>
+				<!-- /.conainer-fluid -->
+		</main>
 
-    <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="http://localhost/dataTabls-css-for-bootstrap4/dataTables.bootstrap4.js"></script>
+		<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+		<script type="text/javascript" src="http://localhost/dataTabls-css-for-bootstrap4/dataTables.bootstrap4.js"></script>
 
-   <script type='text/javascript'>//<![CDATA[
-   	window.onload=function(){
-   		$(document).ready(function() {
+	 <script type='text/javascript'>//<![CDATA[
+		window.onload=function(){
+			$(document).ready(function() {
 
-   			var oldStart = 0;
-   			var oTable = $('#example').DataTable({
-   				"serverSide": true,
-   				"ordering": false,
-   				"ajax": "<?php echo site_url('json/pesanan/' . $juragan . '/' . $status); ?>",
-   				"columns": [
-   				{
-   					"data": "id",
-   					"createdCell": function (td, cellData, rowData, row, col) {
-   						if ( rowData.tanggal.cek_kirim == null ) {
-   							$(td).addClass('bg-warning')
-   						}
-   					}
-   				},
-   				{ 
-   					"data": "juragan",
-   					"render": function ( data, type, full, meta ) {
-   						var uri = "<?php echo site_url('admin/pesanan/lihat'); ?>/";
-   						return '<a href="'+uri+data.username+'">'+data.nama+'</a>';
-   					}
-   				},
-   				{
-   					"data": "tanggal.submit",
-   					"render": function ( data, type, full, meta ) {
-   						var r = data.split(' ');
-   						return '<abbr title="'+ data +'">' + r[0] + '</abbr>';
-   					}
-   				},
-   				{
-   					"data": "pemesan",
-   					"render": function ( data, type, full, meta ) {
-   						var nama = '<strong>'+data.nama+'</strong><br/>';
-   						var hp1 = '<span class="tag tag-info">'+data.hp[0]+'</span>';
-   						var hp2 = '';
-   						var alamat = '<br/>'+data.alamat+'';
-   						if(data.hp[1] != undefined) {
-   							hp2 = '<span class="sr-only"> / </span><span class="tag tag-info">'+data.hp[1]+'</span>';
-   						}
+				var oldStart = 0;
+				var oTable = $('#example').DataTable({
+					"serverSide": true,
+					"ordering": false,
+					"ajax": "<?php echo site_url('json/pesanan/' . $juragan . '/' . $status); ?>",
+					"columns": [
+					{
+						"data": "id",
+						"createdCell": function (td, cellData, rowData, row, col) {
+							if ( rowData.tanggal.cek_kirim == null ) {
+								$(td).addClass('bg-warning')
+							}
+						}
+					},
+					{ 
+						"data": "juragan",
+						"render": function ( data, type, full, meta ) {
+							var uri = "<?php echo site_url('admin/pesanan/lihat'); ?>/";
+							return '<a href="'+uri+data.username+'">'+data.nama+'</a>';
+						}
+					},
+					{
+						"data": "tanggal.submit",
+						"render": function ( data, type, full, meta ) {
+							var r = data.split(' ');
+							return '<abbr title="'+ data +'">' + r[0] + '</abbr>';
+						}
+					},
+					{
+						"data": "pemesan",
+						"render": function ( data, type, full, meta ) {
+							var nama = '<strong>'+data.nama+'</strong><br/>';
+							var hp1 = '<span class="tag tag-info">'+data.hp[0]+'</span>';
+							var hp2 = '';
+							var alamat = '<br/>'+data.alamat+'';
+							if(data.hp[1] != undefined) {
+								hp2 = '<span class="sr-only"> / </span><span class="tag tag-info">'+data.hp[1]+'</span>';
+							}
 
-   						return nama + hp1 + hp2 + alamat;
-   					}
-   				},
-   				{ "data": "biaya.harga" },
-   				{
-            "data": "biaya",
-            "render": function (data, type, full, meta) {
-              var biaya = '<div>biaya: <span class="tag tag-warning">'+data.harga+'</span></div>';
-              var ongkir = '<div>ongkir: <span class="tag tag-success">' +data.ongkir+ '</span></div>';
-              var ongkir_fix = '<div>ongkir fix: <span class="tag tag-info">' +data.ongkir_fix+ '</span></div>';
-              var dp = '<div>transfer: <span class="tag tag-danger">' +data.transfer1+ '</span></div>';
+							return nama + hp1 + hp2 + alamat;
+						}
+					},
+					{ "data": "biaya.harga" },
+					{
+						"data": "biaya",
+						"render": function (data, type, full, meta) {
+							var biaya = '<div>biaya: <span class="tag tag-warning">'+data.harga+'</span></div>';
+							var ongkir = '<div>ongkir: <span class="tag tag-success">' +data.ongkir+ '</span></div>';
+							var ongkir_fix = '<div>ongkir fix: <span class="tag tag-info">' +data.ongkir_fix+ '</span></div>';
+							var dp = '<div>transfer: <span class="tag tag-danger">' +data.transfer1+ '</span></div>';
 
-              if(data.ongkir < 100) {
-                ongkir = '<div><span class="tag tag-success">FREE ONGKIR</span></div>';
-              }
+							if(data.ongkir < 100) {
+								ongkir = '<div><span class="tag tag-success">FREE ONGKIR</span></div>';
+							}
 
-              if(data.ongkir_fix < 100) {
-                ongkir_fix = '';
-              }
-              return biaya + ongkir + ongkir_fix + dp;
-            },
-            "width": "15%"
-          },
-   				{ "data": "pesanan.keterangan"},
-   				{
-   					"data": "pengiriman",
-   					"render": function ( data, type, full, meta ) {
-   						var inf = 'terkirim: ';
-   						if (data.kurir === 'COD') {
-   							var inf = 'terambil: ';
-   						}
-   						return '<strong>' + data.kurir + '</strong><br/>' + data.resi + '<br/><small><em>' + inf + '<abbr title="' + data.tanggal + '">' + data.tanggal_display + '</abbr></em></small>';
+							if(data.ongkir_fix < 100) {
+								ongkir_fix = '';
+							}
+							return biaya + ongkir + ongkir_fix + dp;
+						},
+						"width": "15%"
+					},
+					{ "data": "pesanan.keterangan"},
+					{
+						"data": "pengiriman",
+						"render": function ( data, type, full, meta ) {
+							var inf = 'terkirim: ';
+							if (data.kurir === 'COD') {
+								var inf = 'terambil: ';
+							}
+							return '<strong>' + data.kurir + '</strong><br/>' + data.resi + '<br/><small><em>' + inf + '<abbr title="' + data.tanggal + '">' + data.tanggal_display + '</abbr></em></small>';
 							},
-              "width": "12%"
+							"width": "12%"
 						}
 						],
 						"bLengthChange": false,
@@ -168,29 +170,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						}
 					});
 
-   			$('#searchclear').hide();
-   			$("#searchclear").click(function(){
-   				$("#myInputTextField").val('');
-   				$(this).hide();
-   				oTable.search('').draw();
-   			});
+				$('#searchclear').hide();
+				$("#searchclear").click(function(){
+					$("#myInputTextField").val('');
+					$(this).hide();
+					oTable.search('').draw();
+				});
 
-   			$('#myInputTextField').bind('keyup',function(){
-   				oTable.search($(this).val()).draw() ;
+				$('#myInputTextField').bind('keyup',function(){
+					oTable.search($(this).val()).draw() ;
 
-   				if( ! $(this).val()){
-   					$('#searchclear').hide();
-   				}
-   				else {
-   					$('#searchclear').show();
-   				}
-   			});
+					if( ! $(this).val()){
+						$('#searchclear').hide();
+					}
+					else {
+						$('#searchclear').show();
+					}
+				});
 
-   			$('#length_change').change( function() { 
-   				oTable.page.len( $(this).val() ).draw();
-   			});
+				$('#length_change').change( function() { 
+					oTable.page.len( $(this).val() ).draw();
+				});
 
-   		} );
+			} );
 
 		}//]]> 
 	</script>
