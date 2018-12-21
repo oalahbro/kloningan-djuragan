2b5380cb10839af44661631231c1e91281178d09
Toto Prayogo <mylastof@gmail.com>
2018-12-21 14:39:14 +0700

    SKIP using mysqli

--- app/limited/config/development/database.php
+++ app/limited/config/development/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost;dbname=crows_juragan_1',
+	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'root',
-	'database' => '',
-	'dbdriver' => 'pdo',
+	'database' => 'crows_juragan_1',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
