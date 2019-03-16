4f5e01c37a992ca5012f4e14bcd241e4fa5f90ab
Toto Prayogo <mylastof@gmail.com>
2019-03-17 02:28:57 +0700

    SKIP domain base_url (production)

--- app/limited/config/production/config.php
+++ app/limited/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://juragan.onlinesukses.com/new/';
+$config['base_url'] = 'https://djuragan.com/new/';
 
 /*
 |--------------------------------------------------------------------------
