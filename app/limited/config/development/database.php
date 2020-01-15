e13e3225579c87498083ee80d0b4903a246add9d
Toto Prayogo <mylastof@gmail.com>
2020-01-15 19:49:38 +0700

    _SKIP update db for local

--- app/limited/config/development/database.php
+++ app/limited/config/development/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost;dbname=db_djuragan',
+	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'toto',
 	'password' => 'root',
-	//'database' => 'db_juragan',
-	'dbdriver' => 'pdo',
+	'database' => 'db_djuragan',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
