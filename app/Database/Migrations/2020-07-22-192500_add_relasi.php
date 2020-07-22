3662e7822290f5c347797705714ec296562c82a2
totoprayogo1916 <mylastof@gmail.com>
2020-07-22 19:42:35 +0700

    merge relation for
    juragan-user
    juragan-bank

--- app/Database/Migrations/2020-07-22-192500_add_relasi.php
+++ app/Database/Migrations/2020-07-22-192500_add_relasi.php
@@ -1,6 +1,6 @@
 <?php namespace App\Database\Migrations;
 
-class AddUserRelasi extends \CodeIgniter\Database\Migration {
+class AddRelasi extends \CodeIgniter\Database\Migration {
 
 	public function up()
 	{
@@ -11,12 +11,17 @@ class AddUserRelasi extends \CodeIgniter\Database\Migration {
 				'unsigned' 		 => TRUE,
 				'auto_increment' => TRUE
 			],
+			'table' => [
+				'type' 			 => 'INT',
+				'constraint' 	 => 5, // 1: juragan-user, 2: juragan-bank
+				'unsigned' 		 => TRUE,
+			],
 			'juragan_id' => [
 				'type' 			 => 'INT',
 				'constraint' 	 => 5,
 				'unsigned' 		 => TRUE,
 			],
-			'user_id' => [
+			'val_id' => [
 				'type' 			 => 'INT',
 				'constraint' 	 => 5,
 				'unsigned' 		 => TRUE,
@@ -24,11 +29,11 @@ class AddUserRelasi extends \CodeIgniter\Database\Migration {
 		]);
 		
 		$this->forge->addKey('id_relasi', TRUE);
-		$this->forge->createTable('user_relasi', TRUE);
+		$this->forge->createTable('relasi', TRUE);
 	}
 
 	public function down()
 	{
-		$this->forge->dropTable('user_relasi');
+		$this->forge->dropTable('relasi');
 	}
 }
