0fc940499c7fdacfe13a06094d41b5428d7cac69
totoprayogo1916 <mylastof@gmail.com>
2020-09-07 22:10:30 +0700

    Update Pelanggan.php
    only AJAX

--- app/Controllers/Pelanggan.php
+++ app/Controllers/Pelanggan.php
@@ -2,84 +2,81 @@
 
 namespace App\Controllers;
 
-use App\Models\PelangganModel;
 use App\Libraries\Ongkir;
+use App\Models\PelangganModel;
 
 class Pelanggan extends BaseController
 {
-    public function __construct()
+    public function baru()
     {
-        $session = \Config\Services::session();
+        if (!$this->isLogged()) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
 
-        if ($session->has('logged')) {
-            if (!$session->get('logged')) {
-                return redirect()->to('/auth');
+        if ($this->request->isAJAX()) {
+
+            $ongkir = new Ongkir();
+            $pelanggan     = new PelangganModel();
+
+            $d['nama_pelanggan'] = strtoupper($this->request->getPost('nama_pelanggan'));
+            $d['hp']            = json_encode(array_filter($this->request->getPost('hp')));
+
+            $cod = $this->request->getVar('cod');
+            $cod = ($cod === NULL ? '0' : '1');
+
+            $d['cod']           = $cod;
+            if ($cod === '1') {
+                $d['kecamatan']     = NULL;
+                $d['kabupaten']     = NULL;
+                $d['provinsi']      = NULL;
+                $d['alamat']        = NULL;
+                $d['kodepos']       = NULL;
+            } else {
+                $d['kecamatan']     = $this->request->getPost('kecamatan');
+                $d['kabupaten']     = $this->request->getPost('kabupaten');
+                $d['provinsi']      = $this->request->getPost('provinsi');
+                $d['alamat']        = strtoupper($this->request->getPost('alamat'));
+                $d['kodepos']       = $this->request->getPost('kodepos');
             }
+
+            if ($this->request->getVar('id_pelanggan')) {
+                $d['id_pelanggan'] = (int) $this->request->getPost('id_pelanggan');
+            }
+
+            $pelanggan->save($d);
+
+            if ($this->request->getVar('id_pelanggan')) {
+                $id = (int) $this->request->getPost('id_pelanggan');
+            } else {
+                $db = \Config\Database::connect();
+                $id = $db->insertID();
+            }
+
+            $result = $pelanggan->ambil($id)->getFirstRow();
+            $kota = $ongkir->kota($result->provinsi, $result->kabupaten);
+
+            $r['id_pelanggan'] = (int) $result->id_pelanggan;
+            $r['hp'] = json_decode($result->hp);
+            $r['nama_pelanggan'] = $result->nama_pelanggan;
+            $r['cod'] = $result->cod;
+
+            if ($result->cod === '0') {
+                $r['nama_kecamatan'] = strtoupper($ongkir->kecamatan($result->kabupaten, $result->kecamatan)['subdistrict_name']);
+                $r['kecamatan'] = (int) $result->kecamatan;
+                $r['nama_kabupaten'] = strtoupper(($kota['type'] === 'Kabupaten' ? '' : '(Kota) ') . $kota['city_name']);
+                $r['kabupaten'] = (int) $result->kabupaten;
+                $r['nama_provinsi'] = strtoupper($ongkir->provinsi($result->provinsi)['province']);
+                $r['provinsi'] = (int) $result->provinsi;
+                $r['alamat'] = $result->alamat;
+                $r['kodepos'] = $result->kodepos;
+            }
+
+            return $this->response->setJSON($r);
         } else {
-            return redirect()->to('/auth');
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
         }
     }
 
     // ------------------------------------------------------------------------
 
-    public function baru()
-    {
-        $ongkir = new Ongkir();
-        $pelanggan     = new PelangganModel();
-
-        $d['nama_pelanggan'] = strtoupper($this->request->getPost('nama_pelanggan'));
-        $d['hp']            = json_encode(array_filter($this->request->getPost('hp')));
-
-        $cod = $this->request->getVar('cod');
-        $cod = ($cod === NULL ? '0' : '1');
-
-        $d['cod']           = $cod;
-        if ($cod === '1') {
-            $d['kecamatan']     = NULL;
-            $d['kabupaten']     = NULL;
-            $d['provinsi']      = NULL;
-            $d['alamat']        = NULL;
-            $d['kodepos']       = NULL;
-        } else {
-            $d['kecamatan']     = $this->request->getPost('kecamatan');
-            $d['kabupaten']     = $this->request->getPost('kabupaten');
-            $d['provinsi']      = $this->request->getPost('provinsi');
-            $d['alamat']        = strtoupper($this->request->getPost('alamat'));
-            $d['kodepos']       = $this->request->getPost('kodepos');
-        }
-
-        if ($this->request->getVar('id_pelanggan')) {
-            $d['id_pelanggan'] = (int) $this->request->getPost('id_pelanggan');
-        }
-
-        $pelanggan->save($d);
-
-        if ($this->request->getVar('id_pelanggan')) {
-            $id = (int) $this->request->getPost('id_pelanggan');
-        } else {
-            $db = \Config\Database::connect();
-            $id = $db->insertID();
-        }
-
-        $result = $pelanggan->ambil($id)->getFirstRow();
-        $kota = $ongkir->kota($result->provinsi, $result->kabupaten);
-
-        $r['id_pelanggan'] = (int) $result->id_pelanggan;
-        $r['hp'] = json_decode($result->hp);
-        $r['nama_pelanggan'] = $result->nama_pelanggan;
-        $r['cod'] = $result->cod;
-
-        if ($result->cod === '0') {
-            $r['nama_kecamatan'] = strtoupper($ongkir->kecamatan($result->kabupaten, $result->kecamatan)['subdistrict_name']);
-            $r['kecamatan'] = (int) $result->kecamatan;
-            $r['nama_kabupaten'] = strtoupper(($kota['type'] === 'Kabupaten' ? '' : '(Kota) ') . $kota['city_name']);
-            $r['kabupaten'] = (int) $result->kabupaten;
-            $r['nama_provinsi'] = strtoupper($ongkir->provinsi($result->provinsi)['province']);
-            $r['provinsi'] = (int) $result->provinsi;
-            $r['alamat'] = $result->alamat;
-            $r['kodepos'] = $result->kodepos;
-        }
-
-        return $this->response->setJSON($r);
-    }
 }
