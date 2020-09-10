b787683f4d9d166ad9cf1e9aecf855e3efa8a76e
totoprayogo1916 <mylastof@gmail.com>
2020-09-10 12:20:59 +0700

    Update JuraganModel.php
    - tambah fungsi terakhir_update()
    - reformat

--- app/Models/JuraganModel.php
+++ app/Models/JuraganModel.php
@@ -29,7 +29,7 @@ class JuraganModel extends Model
 
 	public function ambil()
 	{
-		$builder = $this->db->table('juragan j');
+		$builder = $this->db->table($this->table . ' j');
 		$builder->select('j.*');
 
 		$builder->select('CONCAT("[" ,GROUP_CONCAT(DISTINCT CONCAT("{","&quot;id&quot;:",b.id_bank,",","&quot;nama&quot;:&quot;",b.nama_bank,"&quot;,","&quot;no&quot;:&quot;",b.rekening,"&quot;,","&quot;atas_nama&quot;:&quot;",b.atas_nama,"&quot;}")),"]") as bank');
@@ -44,20 +44,38 @@ class JuraganModel extends Model
 
 	// ------------------------------------------------------------------------
 
-    public function ambil_bank($juragan_id)
-    {
-		$bank = $this->db->table('juragan j');
+	public function ambil_bank($juragan_id)
+	{
+		$bank = $this->db->table($this->table . ' j');
 
 		$bank->join('relasi r', 'r.juragan_id = j.id_juragan', 'left');
 		$bank->join('bank b', 'b.id_bank = r.val_id', 'left');
-		
+
 		$bank->where('j.id_juragan', $juragan_id);
 
 		$bank->orderBy('b.id_bank', 'DESC');
 
 		return $bank->get();
-    }
+	}
 
-    // ------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
 
+	public function terakhir_update($ids_juragan)
+	{
+		if(is_array($ids_juragan)) {
+			$juragan = $this->db->table($this->table . ' j');
+			$juragan->select('i.juragan_id as id_juragan, j.*');
+			$juragan->join('relasi r', 'r.juragan_id = j.id_juragan', 'left');
+			$juragan->join('user u', 'u.id = r.val_id', 'left');
+			$juragan->join('invoice i', 'i.juragan_id = j.id_juragan', 'left');
+
+			$juragan->havingIn('i.juragan_id', $ids_juragan);
+			$juragan->orderBy('i.update_at', 'ASC');
+
+			return $juragan->get()->getLastRow();
+		}
+		else {
+			return ['error' => '$ids_juragan harus array'];
+		}
+	}
 }
