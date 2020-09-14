29728fc5838798430105fa6575da4bcf5f8bfb09
totoprayogo1916 <mylastof@gmail.com>
2020-09-14 16:00:14 +0700

    penambahan config kurir

--- app/Config/JuraganConfig.php
+++ app/Config/JuraganConfig.php
@@ -1,4 +1,6 @@
-<?php namespace Config;
+<?php
+
+namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
 
@@ -7,18 +9,18 @@ class JuraganConfig extends BaseConfig
 	public $rajaongkir  = '75f538ed88e26297a2fabed240ed8bf0'; // ID rajaongkir
 
 	public $label = array(
-		 '1' => 'Blibli',
-		 '12' => 'OLX',
-		 '2' => 'Bukalapak',
-		 '3' => 'Facebook',
-		 '4' => 'Instagram',
-		 '5' => 'Lazada',
-		 '7' => 'Shopee',
-		 '11' => 'Tokopedia',
-		 '9' => 'WhatsApp',
-		 '10' => 'Zalora',
-		 '8' => 'Web/App lain',
-		 '6' => 'Offline Store/COD'
+		'1' => 'Blibli',
+		'12' => 'OLX',
+		'2' => 'Bukalapak',
+		'3' => 'Facebook',
+		'4' => 'Instagram',
+		'5' => 'Lazada',
+		'7' => 'Shopee',
+		'11' => 'Tokopedia',
+		'9' => 'WhatsApp',
+		'10' => 'Zalora',
+		'8' => 'Web/App lain',
+		'6' => 'Offline Store/COD'
 	);
 
 	public $size = array(
@@ -41,4 +43,16 @@ class JuraganConfig extends BaseConfig
 		)
 		// custom
 	);
+
+	public $kurir = [
+		'POS',
+		'JNE',
+		'COD',
+		'First Logistic',
+		'TIKI',
+		'Wahana',
+		'J&T',
+		'RPX',
+		'lainnya'
+	];
 }
