e83badea02622e835a4b8e45294ed7dc32b9bf12
totoprayogo1916 <totoprayogo1916@gmail.com>
2020-10-07 00:19:25 +0700

    for CLI

---
+++ app/Controllers/Notifikasi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Notifikasi extends Controller
+{
+    public function hapus()
+    {
+        $notifikasi = new \App\Models\NotifikasiModel();
+
+        $builder = $notifikasi->builder();
+        $builder->where('read_at !=', NULL);
+        $counting = $builder->countAllResults();
+
+        $notifikasi->where('read_at !=', NULL)->delete();
+
+        return $this->response->setJSON(['status' => 200, 'message' => $counting . ' notifikasi sudah dihapus']);
+    }
+}
