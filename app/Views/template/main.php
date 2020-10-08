dfc1980c495687ddedf30580698510d63278cfa2
totoprayogo1916 <totoprayogo1916@gmail.com>
2020-10-08 23:35:55 +0700

    css gunakan scss bootstrap

--- app/Views/template/main.php
+++ app/Views/template/main.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>" />
+	<link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>" />
 
 	<title><?= $title; ?></title>
 </head>
@@ -15,9 +15,7 @@
 <body>
 	<?= $this->renderSection('content') ?>
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<!-- JavaScript -->
 	<script src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
 </body>
 
