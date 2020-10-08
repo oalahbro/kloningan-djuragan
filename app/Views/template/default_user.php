9f478c268507f3928bbc2e78ac2c875c08f8a74f
totoprayogo1916 <totoprayogo1916@gmail.com>
2020-10-09 00:23:14 +0700

    restore

--- app/Views/template/default_user.php
+++ app/Views/template/default_user.php
@@ -89,7 +89,9 @@ $JSmin = new Minify\JS();
 			<div class="list-group list-group-flush" id="notifDisini"></div>
 		</div>
 	</div>
-	<!-- JavaScript -->
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->	
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script defer src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
 
 	<?php /*
--- app/Views/template/main.php
+++ app/Views/template/main.php
@@ -15,7 +15,9 @@
 <body>
 	<?= $this->renderSection('content') ?>
 
-	<!-- JavaScript -->
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->	
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script src="<?= base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
 </body>
 
